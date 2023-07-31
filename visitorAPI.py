ccb1a39ea8c4fa40665762f05616560c0339b069
allenlee-37 <watersurface37@gmail.com>
2023-07-31 13:23:28 +0900

    script in python

--- visitorAPI.py
+++ visitorAPI.py
@@ -1,63 +1,76 @@
+import argparse
 import requests
 import json 
 import pandas as pd
-import argparse
+from tqdm import tqdm
 
 
 
-# url을 가져오되 광역지자체 대상인지, 지역지자체 대상인지 지정 필요
-def get_url(target, numOfRows, startYmd, endYmd):
-    if target == 'metco':
-        api_name = "metcoRegnVisitrDDList"
-    elif target == 'locgo':
-        api_name = "locgoRegnVisitrDDList"  
+def request_url(numOfRows=100, 
+                pageNo=1, 
+                MobileOS="ETC", 
+                MobileApp="AppTest", 
+                startYmd=20220101, 
+                endYmd=20221231):
     end_point = "https://apis.data.go.kr/B551011/DataLabService"
-    pageNo = 1
-    MobileOS = "ETC"
-    MobileApp = "AppTest"
+    api_name = "locgoRegnVisitrDDList"
     service_key = "I%2BxJPDnE7MUOEc7%2Fz6XlywW7hcFOSfkHW6mk8wzlVr6w7sdiRYmOrp4XZrOsZ2vnt5AB%2BVgGLLVu%2BuXN8ojeZQ%3D%3D"
-    d_type = 'json'
+    d_type = "json"
     url = f"{end_point}/{api_name}?numOfRows={numOfRows}&pageNo={pageNo}&MobileOS={MobileOS}&MobileApp={MobileApp}&serviceKey={service_key}&_type={d_type}&startYmd={startYmd}&endYmd={endYmd}"
+    
     return url
 
-def total_row(url):
-    response = requests.get(url)
-    json_text = response.text
-    js = json.loads(json_text)
-    return js['response']['body']['totalCount']
-
-
-def get_json(numRow, startYmd, endYmd):
-    url = request_url(numOfRows=numRow,startYmd=startYmd, endYmd=endYmd)
+def get_total_row(startYmd, endYmd):
+    url = request_url(numOfRows=1, pageNo= 1, startYmd=startYmd, endYmd=endYmd)
     response = requests.get(url)
     json_text = response.text
     json_result = json.loads(json_text)
     total_row = json_result['response']['body']['totalCount']
-    print(f'총 열 갯수: {total_row}')
-    print(f'지정된 열 갯수: {numRow}')
-    return json_result
+    print(f'전체 row 수: {total_row}')
+    return total_row
 
-js = get_json(numRow=target_row,startYmd=start_date,endYmd=end_date)
-
-
+def get_json(startYmd, endYmd, total_row):
+    def page_reader(page):
+        url = request_url(numOfRows=1000, pageNo=page, startYmd=startYmd, endYmd=endYmd)
+        json_text = requests.get(url).text
+        json_result = json.loads(json_text)
+        pre_df = json_result['response']['body']['items']['item']
+        df = pd.json_normalize(pre_df)
+        df = df.sort_values(by=['signguCode', 'baseYmd', 'touDivCd'], ascending=True)
+        df = df.reset_index(drop=True)
+        df = df[['signguCode', 'touDivCd', 'touNum', 'baseYmd']]
+        
+        # df = df[df['signguCode']=='47190']
+        return df
+    
+    result = page_reader(0)
+    for page in tqdm(range(1, int(total_row)//1000)):
+        df = page_reader(page)
+        result = pd.concat([result, df])
+        if page%10==0:
+            result.to_excel(f'result/{startYmd}-{endYmd}-{page} - 기초 지자체 지역방문자수 집계 데이터 정보 조회.xlsx')
+            print(f'저장 완료 - {startYmd}-{endYmd}-{page} - 기초 지자체 지역방문자수 집계 데이터 정보 조회')
+    result.to_excel(f'result/{startYmd}-{endYmd}-{page} - 기초 지자체 지역방문자수 집계 데이터 정보 조회.xlsx')
+    return result
 
 def main():
-    p = argparse.ArgumentParser()
-    p.add_argument('--startDate', '-s', type=int, required=True)
-    p.add_argument('--endDate', '-e', type=int, required=True)
-    args = p.parse_args()
+    parser = argparse.ArgumentParser(
+        prog = '한국관광공사 API',
+        description = '한국관광공사 방문자 수 API 호출'
+    )
+    parser.add_argument('-s', '--startdate', type=int, help='시작하는 날짜')
+    parser.add_argument('-e', '--enddate', type=int, help='끝나는 날짜')
+
+    args = parser.parse_args()
+
+    print(args.startdate)
+    print(args.enddate)
     
-    start_date = args.startDate
-    end_date = args.endDate
+    start_date = args.startdate
+    end_date = args.enddate
 
-    metco_url = get_url('metgo',1,start_date,end_date)
-    locgo_url = get_url('locgo',1,start_date,end_date)
-
-    metco_total = total_row(metco_url)
-    locgo_total = total_row(locgo_url)
-    
-    request_url()
-
-    pass
+    total_row = get_total_row(start_date, end_date)
+    result = get_json(start_date, end_date, total_row)
 
 if __name__ == "__main__": main()
+   
